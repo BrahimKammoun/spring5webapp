1a380ddf9ba52f3a7c2647e178c4027bf3b47763
BrahimKammoun <brahimkammoun@gmail.com>
2019-01-22 13:26:12 +0100

    Adding Code for Author

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -16,6 +16,7 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+    private int code;
 
     @ManyToMany (mappedBy = "authors")
     private Set<Book> books  = new HashSet<>();
